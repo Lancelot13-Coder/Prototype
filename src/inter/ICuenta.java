645a702407726dd9e5ddccfa6cc224f511e375d2
Lancelot <[200~>
2025-04-11 07:46:18 -0500

    Prototype

---
+++ src/inter/ICuenta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package inter;
+
+/**
+ *
+ * @author Estudiantes
+ */
+
+    public interface ICuenta extends Cloneable{
+            
+            ICuenta clonar ();
+                    }
+
